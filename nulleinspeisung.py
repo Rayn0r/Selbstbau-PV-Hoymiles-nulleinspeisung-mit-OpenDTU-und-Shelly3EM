65dbf563c2b264a5dba1216fb213d36f117616ea
Rayn0r <4147858+Rayn0r@users.noreply.github.com>
2023-11-26 21:50:45 +0100

    Erweiterung/Umbau für Shelly EM3 Pro

--- nulleinspeisung.py
+++ nulleinspeisung.py
@@ -1,71 +1,138 @@
 #!/usr/bin/env python3
-import requests, time, sys
-from requests.auth import HTTPBasicAuth
+"""
+Ein Python-Skript, das den aktuellen Hausverbrauch aus einem Shelly 3EM (Pro) ausliest,
+die Nulleinspeisung berechnet und die Ausgangsleistung eines Hoymiles-Wechselrichters
+mit Hilfe der OpenDTU entsprechend anpasst.
+Somit wird kein unnötiger Strom ins Betreibernetz abgegeben.
+"""
 
+import time
+import sys
+import json
+from requests.auth import HTTPBasicAuth
+import requests
 
 # Diese Daten müssen angepasst werden:
-serial = "112100000000" # Seriennummer des Hoymiles Wechselrichters
-maximum_wr = 300 # Maximale Ausgabe des Wechselrichters
-minimum_wr = 100 # Minimale Ausgabe des Wechselrichters
+SERIAL = "112100000000" # Seriennummer des Hoymiles Wechselrichters
+MAXIMUM_WR = 300 # Maximale Ausgabe des Wechselrichters
+MINIMUM_WR = 100 # Minimale Ausgabe des Wechselrichters
 
-dtu_ip = '192.100.100.20' # IP Adresse von OpenDTU
-dtu_nutzer = 'admin' # OpenDTU Nutzername
-dtu_passwort = 'openDTU42' # OpenDTU Passwort
+DTU_IP = '192.100.100.20' # IP Adresse von OpenDTU
+DTU_NUTZER = 'admin' # OpenDTU Nutzername
+DTU_PASSWORT = 'openDTU42' # OpenDTU Passwort
 
-shelly_ip = '192.100.100.30' # IP Adresse von Shelly 3EM
+SHELLY_IP = '192.100.100.30' # IP Adresse von Shelly 3EM
 
+STANDARD_TIMEOUT = 5    # Zeit in Sekunden
+EM3PRO = True           # Wird ein Shelly EM3 Pro eingesetzt?
+
+def get_request(url_name, header_definition=''):
+    """
+    Retrieve simple value from URL and return it
+    in case things go wrong, return False
+    """
+    try:
+        daten = requests.get(url = url_name, timeout=STANDARD_TIMEOUT,
+                    headers=header_definition ).json()
+        return daten
+    except Exception as fehler:
+        print(f"Fehler {fehler} beim Aufruf von {url_name}")
+        return False
+
+def post_request(url_name, post_daten, auth_daten='', header_definition=''):
+    """
+    send post request to 
+    """
+    try:
+        daten = requests.post(url_name, data=json.dumps(post_daten),
+                    timeout=STANDARD_TIMEOUT, auth=auth_daten, headers=header_definition).json()
+        return daten
+    except Exception as fehler:
+        print(f"Fehler {fehler} beim Aufruf von {url_name}")
+        return False
 
 while True:
-    try:
-        # Nimmt Daten von der openDTU Rest-API und übersetzt sie in ein json-Format
-        r = requests.get(url = f'http://{dtu_ip}/api/livedata/status/inverters' ).json()
-
+    # Nimmt Daten von der openDTU Rest-API und übersetzt sie in ein json-Format
+    r_dtu = get_request(f'http://{DTU_IP}/api/livedata/status/inverters')
+    if r_dtu:
         # Selektiert spezifische Daten aus der json response
-        reachable   = r['inverters'][0]['reachable'] # Ist DTU erreichbar?
-        producing   = int(r['inverters'][0]['producing']) # Produziert der Wechselrichter etwas?
-        altes_limit = int(r['inverters'][0]['limit_absolute']) # Altes Limit
-        power_dc    = r['inverters'][0]['AC']['0']['Power DC']['v']  # Lieferung DC vom Panel
-        power       = r['inverters'][0]['AC']['0']['Power']['v'] # Abgabe BKW AC in Watt
-    except:
-        print('Fehler beim Abrufen der Daten von openDTU')
-    try:
-        # Nimmt Daten von der Shelly 3EM Rest-API und übersetzt sie in ein json-Format
-        phase_a     = requests.get(f'http://{shelly_ip}/emeter/0', headers={'Content-Type': 'application/json'}).json()['power']
-        phase_b     = requests.get(f'http://{shelly_ip}/emeter/1', headers={'Content-Type': 'application/json'}).json()['power']
-        phase_c     = requests.get(f'http://{shelly_ip}/emeter/2', headers={'Content-Type': 'application/json'}).json()['power']
-        grid_sum    = phase_a + phase_b + phase_c # Aktueller Bezug - rechnet alle Phasen zusammen
-    except:
-        print('Fehler beim Abrufen der Daten von Shelly 3EM')
+        reachable   = r_dtu['inverters'][0]['reachable'] # Ist DTU erreichbar?
+        producing   = int(r_dtu['inverters'][0]['producing']) # Produziert der Wechselrichter etwas?
+        altes_limit = int(r_dtu['inverters'][0]['limit_absolute']) # Altes Limit
+        power_dc    = r_dtu['inverters'][0]['AC']['0']['Power DC']['v']  # Lieferung DC vom Panel
+        power       = r_dtu['inverters'][0]['AC']['0']['Power']['v'] # Abgabe BKW AC in Watt
+        # print(f"{reachable} {producing} {altes_limit} {power_dc} {power}")
+    else:
+        print("Konnte keine Daten von OpenDTU empfangen.")
+        time.sleep(STANDARD_TIMEOUT) # warte STANDARD_TIMEOUT bevor wir es erneut probieren.
+        continue
+
+    # Nimmt Daten von der Shelly 3EM (Pro) Rest-API(2) und übersetzt sie ins json-Format
+    url_api1 = f"http://{SHELLY_IP}/emeter/"
+    url_api2 = f"http://{SHELLY_IP}/rpc"
+    if EM3PRO:
+
+        # hole Echtzeitdaten vom Shelly
+        request_daten = {"id":1,"method":"EM.GetStatus","params":{"id":0}}
+        if request_daten:
+            json_response = post_request(url_api2, request_daten)
+            # print(json.dumps(json_response, indent=4))
+            # print(f"total_act_power: {json_response['result']['total_act_power']}")
+            grid_sum=json_response['result']['total_act_power']
+        else:
+            print("Konnte keine Daten von Shelly EM3 Pro empfangen.")
+            time.sleep(STANDARD_TIMEOUT) # warte STANDARD_TIMEOUT bevor wir es erneut probieren.
+            continue
+    else:
+        phase_a = get_request(f'{url_api1}/0',
+                    {'Content-Type': 'application/json'})['power']
+        if phase_a:
+            phase_b = get_request(f'{url_api1}/1',
+                        {'Content-Type': 'application/json'})['power']
+        else:
+            print("Konnte keine Daten von Shelly EM3 empfangen.")
+            time.sleep(STANDARD_TIMEOUT) # warte STANDARD_TIMEOUT bevor wir es erneut probieren.
+            continue
+        if phase_b:
+            phase_c = get_request(f'{url_api1}/2',
+                        {'Content-Type': 'application/json'})['power']
+            grid_sum = phase_a + phase_b + phase_c # Aktueller Bezug - rechnet alle Phasen zusammen
+        else:
+            print("Konnte keine Daten von Shelly EM3 empfangen.")
+            time.sleep(STANDARD_TIMEOUT) # warte STANDARD_TIMEOUT bevor wir es erneut probieren.
+            continue
 
     # Werte setzen
-    print(f'\nBezug: {round(grid_sum, 1)} W, Produktion: {round(power, 1)} W, Verbrauch: {round(grid_sum + power, 1)} W')
+    print(f'\nBezug: {round(grid_sum, 1)} W, Produktion: {round(power, 1)} W,\
+      Verbrauch: {round(grid_sum + power, 1)} W')
     if reachable:
-        setpoint = grid_sum + altes_limit - 5 # Neues Limit in Watt
+        SET_POINT = grid_sum + altes_limit - 5 # Neues Limit in Watt
 
         # Fange oberes Limit ab
-        if setpoint > maximum_wr:
-            setpoint = maximum_wr
-            print(f'Setpoint auf Maximum: {maximum_wr} W')
+        if SET_POINT > MAXIMUM_WR:
+            SET_POINT = MAXIMUM_WR
+            print(f'Setpoint auf Maximum: {MAXIMUM_WR} W')
         # Fange unteres Limit ab
-        elif setpoint < minimum_wr:
-            setpoint = minimum_wr
-            print(f'Setpoint auf Minimum: {minimum_wr} W')
+        elif SET_POINT < MINIMUM_WR:
+            SET_POINT = MINIMUM_WR
+            print(f'Setpoint auf Minimum: {MINIMUM_WR} W')
         else:
-            print(f'Setpoint berechnet: {round(grid_sum, 1)} W + {round(altes_limit, 1)} W - 5 W = {round(setpoint, 1)} W')
+            print(f'Setpoint berechnet: {round(grid_sum, 1)} W + {round(altes_limit, 1)
+                } W - 5 W = {round(SET_POINT, 1)} W')
 
-        if setpoint != altes_limit:
-            print(f'Setze Inverterlimit von {round(altes_limit, 1)} W auf {round(setpoint, 1)} W... ', end='')
+        if SET_POINT != altes_limit:
+            print(f'Setze Inverterlimit von {round(altes_limit, 1)
+                } W auf {round(SET_POINT, 1)} W... ', end='')
             # Neues Limit setzen
             try:
-                r = requests.post(
-                    url = f'http://{dtu_ip}/api/limit/config',
-                    data = f'data={{"serial":"{serial}", "limit_type":0, "limit_value":{setpoint}}}',
-                    auth = HTTPBasicAuth(dtu_nutzer, dtu_passwort),
-                    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+                r = post_request(
+                    f'http://{DTU_IP}/api/limit/config',
+                    f'data={{"serial":"{SERIAL}", "limit_type":0, "limit_value":{SET_POINT}}}',
+                    HTTPBasicAuth(DTU_NUTZER, DTU_PASSWORT),
+                    {'Content-Type': 'application/x-www-form-urlencoded'}
                 )
                 print(f'Konfiguration gesendet ({r.json()["type"]})')
-            except:
-                print('Fehler beim Senden der Konfiguration')
-
+            except Exception as Fehler:
+                print(f'Fehler: {Fehler} beim Senden der Konfiguration')
     sys.stdout.flush() # write out cached messages to stdout
     time.sleep(5) # wait
