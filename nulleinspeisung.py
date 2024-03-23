3d3e36de8d49f3b2a5df3aef728e2a3539d9f5dd
Rayn0r <4147858+Rayn0r@users.noreply.github.com>
2024-03-23 08:12:38 +0100

    feat: fix for /api/system/status
    fixed breaking changes in v24.2.12

--- nulleinspeisung.py
+++ nulleinspeisung.py
@@ -33,35 +33,51 @@ def get_request(url_name, header_definition=''):
     """
     try:
         daten = requests.get(url = url_name, timeout=STANDARD_TIMEOUT,
-                    headers=header_definition ).json()
+                headers=header_definition ).json()
         return daten
-    except Exception as fehler:
+    except Exception as fehler: # pylint: disable=broad-except
         print(f"Fehler {fehler} beim Aufruf von {url_name}")
         return False
 
 def post_request(url_name, post_daten, auth_daten='', header_definition=''):
     """
-    send post request to 
+    send post request to url_name and return the request status
+    if things go wrong, return False
     """
     try:
-        daten = requests.post(url_name, data=json.dumps(post_daten),
+        daten = requests.post(url=url_name, data=post_daten,
                     timeout=STANDARD_TIMEOUT, auth=auth_daten, headers=header_definition).json()
         return daten
-    except Exception as fehler:
+    except Exception as fehler: # pylint: disable=broad-except
         print(f"Fehler {fehler} beim Aufruf von {url_name}")
         return False
 
 while True:
     # Nimmt Daten von der openDTU Rest-API und übersetzt sie in ein json-Format
-    r_dtu = get_request(f'http://{DTU_IP}/api/livedata/status/inverters')
-    if r_dtu:
+    r_dtu = get_request(f'http://{DTU_IP}/api/livedata/status')
+
+    # hole den git tag der aktuellen Firmware aus /api/livedata/status
+    system_status = get_request(f'http://{DTU_IP}/api/system/status')
+    if system_status:
+        # hole git tag
+        git_tag = system_status['git_hash'][1:]
+        git_tag_arr = git_tag.split('.')
+        git_tag_arr_i = list(map(int, git_tag_arr))
+
+    if r_dtu and system_status:
+        #print(f"r_dtu:\n{json.dumps(r_dtu, indent=2)}", flush=True)
         # Selektiert spezifische Daten aus der json response
-        reachable   = r_dtu['inverters'][0]['reachable'] # Ist DTU erreichbar?
+        REACHABLE   = r_dtu['inverters'][0]['reachable'] # Ist DTU erreichbar?
         producing   = int(r_dtu['inverters'][0]['producing']) # Produziert der Wechselrichter etwas?
         altes_limit = int(r_dtu['inverters'][0]['limit_absolute']) # Altes Limit
-        power_dc    = r_dtu['inverters'][0]['AC']['0']['Power DC']['v']  # Lieferung DC vom Panel
-        power       = r_dtu['inverters'][0]['AC']['0']['Power']['v'] # Abgabe BKW AC in Watt
-        # print(f"{reachable} {producing} {altes_limit} {power_dc} {power}")
+
+        # das Format von /api/livedata/status hat sich mit v24.2.12 geändert...
+        # https://github.com/tbnobody/OpenDTU/releases/tag/v24.2.12
+        if git_tag_arr_i[0] >= 24 and git_tag_arr_i[1] >= 2 and git_tag_arr_i[2] >= 12:
+            power   = r_dtu['total']['Power']['v'] # Abgabe BKW AC in Watt
+        else:
+            power   = r_dtu['inverters'][0]['AC']['0']['Power']['v'] # Abgabe BKW AC in Watt
+        # print(f"{REACHABLE} {producing} {altes_limit} {power}")
     else:
         print("Konnte keine Daten von OpenDTU empfangen.")
         time.sleep(STANDARD_TIMEOUT) # warte STANDARD_TIMEOUT bevor wir es erneut probieren.
@@ -70,12 +86,13 @@ while True:
     # Nimmt Daten von der Shelly 3EM (Pro) Rest-API(2) und übersetzt sie ins json-Format
     url_api1 = f"http://{SHELLY_IP}/emeter/"
     url_api2 = f"http://{SHELLY_IP}/rpc"
-    if EM3PRO:
+    url_api3 = f"http://{DTU_IP}/api/limit/status"
 
+    if EM3PRO:
         # hole Echtzeitdaten vom Shelly
         request_daten = {"id":1,"method":"EM.GetStatus","params":{"id":0}}
         if request_daten:
-            json_response = post_request(url_api2, request_daten)
+            json_response = post_request(url_api2, json.dumps(request_daten))
             # print(json.dumps(json_response, indent=4))
             # print(f"total_act_power: {json_response['result']['total_act_power']}")
             grid_sum=json_response['result']['total_act_power']
@@ -104,8 +121,18 @@ while True:
 
     # Werte setzen
     print(f'\nBezug: {round(grid_sum, 1)} W, Produktion: {round(power, 1)} W,\
-      Verbrauch: {round(grid_sum + power, 1)} W')
-    if reachable:
+ Verbrauch: {round(grid_sum + power, 1)} W')
+    if REACHABLE:
+        limit_status = get_request(url_api3)
+        if limit_status:
+            limit_status_val=limit_status[SERIAL]['limit_set_status']
+            print(f'limit status: {limit_status_val}')
+            # Der Inverter verrät uns, ob er mit dem Setzen des letzten Wertes fertig geworden ist.
+            # Wir setzen keinen neuen Wert, bevor der Inverter kein 'Ok' liefert.
+            if limit_status_val != 'Ok':
+                print('Letzte Einstellung noch nicht gesetzt. Warte bis zum nächsten Durchlauf...')
+                time.sleep(STANDARD_TIMEOUT) # wait
+                continue
         SET_POINT = grid_sum + altes_limit - 5 # Neues Limit in Watt
 
         # Fange oberes Limit ab
@@ -117,22 +144,26 @@ while True:
             SET_POINT = MINIMUM_WR
             print(f'Setpoint auf Minimum: {MINIMUM_WR} W')
         else:
-            print(f'Setpoint berechnet: {round(grid_sum, 1)} W + {round(altes_limit, 1)
-                } W - 5 W = {round(SET_POINT, 1)} W')
+            print(f'Setpoint berechnet: {round(grid_sum, 1)} W + {round(altes_limit, 1)}\
+ W - 5 W = {round(SET_POINT, 1)} W')
 
-        if SET_POINT != altes_limit:
-            print(f'Setze Inverterlimit von {round(altes_limit, 1)
-                } W auf {round(SET_POINT, 1)} W... ', end='')
+        if abs(SET_POINT - altes_limit) > 2:    # Weil der Wert, den wir setzen nicht 100%
+                                                # der Wert ist den wir auslesen,
+                                                # lassen wir ein Differenz von 2W zu.
+            print(f'Setze Inverterlimit von {round(altes_limit, 1)}\
+ W auf {round(SET_POINT, 1)} W... ', end='')
             # Neues Limit setzen
-            try:
-                r = post_request(
-                    f'http://{DTU_IP}/api/limit/config',
-                    f'data={{"serial":"{SERIAL}", "limit_type":0, "limit_value":{SET_POINT}}}',
-                    HTTPBasicAuth(DTU_NUTZER, DTU_PASSWORT),
-                    {'Content-Type': 'application/x-www-form-urlencoded'}
-                )
-                print(f'Konfiguration gesendet ({r.json()["type"]})')
-            except Exception as Fehler:
-                print(f'Fehler: {Fehler} beim Senden der Konfiguration')
+            LIMIT_IN_PROZENT = (SET_POINT / INVERTER_MAX_POWER) * 100
+            r = post_request(
+                f'http://{DTU_IP}/api/limit/config',
+                f'data={{"serial":"{SERIAL}", "limit_type":0, "limit_value":{SET_POINT}}}',
+                HTTPBasicAuth(DTU_NUTZER, DTU_PASSWORT),
+                {'Content-Type': 'application/x-www-form-urlencoded'}
+            )
+            if r:
+                print(f'Konfiguration gesendet: ({r["type"]}, {r["message"]})')
+            else:
+                print('Fehler: beim Senden der Konfiguration')
+    REACHABLE = False
     sys.stdout.flush() # write out cached messages to stdout
-    time.sleep(5) # wait
+    time.sleep(STANDARD_TIMEOUT) # wait
